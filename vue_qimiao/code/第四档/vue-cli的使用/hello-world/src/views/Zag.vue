72cd0802c7fd64b12b03d629bb5f004a7c0eab4f
Aug <>
2021-04-14 09:47:46 +0800

    使用vue-cli创建项目&路由的使用

---
+++ vue_qimiao/code/第四档/vue-cli的使用/hello-world/src/views/Zag.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="zag">
+    <h1>{{ hello }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "zag",
+  data() {
+    return {
+      hello: "hello zag!!!",
+    };
+  },
+};
+</script>
+
